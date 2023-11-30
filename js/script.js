9691abfaa1bf30b7aeb3c70d7c997b0a163d394e
Eva Haras <usenkoeva1@gmail.com>
2023-11-30 23:59:49 +0200

    HW

--- js/script.js
+++ js/script.js
@@ -1,90 +1,51 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
-            },
-            330:{
-                items:2
-            },
-            480:{
-                items:3
-            },
-            625:{
-                items:4
-            },
-            785:{
-                items:5
-            },
-            935:{
-                items:6
-            },
-            1090:{
-                items:7
-            },
-            1250:{
-                items:8
-            },
-            1290:{
-                items:9
-            }
-        }
-});
-    $( function() {
-        $( "#accordion" ).accordion();
-      } );
-});
-
-
-
-/* Please ❤ this if you like it! */
-
-
-(function($) { "use strict";
-		
-	
-	
-	//Navigation
-
-	var app = function () {
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-			body = document.querySelector('body');
-			menu = document.querySelector('.menu-icon');
-			menuItems = document.querySelectorAll('.nav__list-item');
-			applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-			menu.addEventListener('click', function () {
-				return toggleClass(body, 'nav-active');
-			});
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}();
-
-	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("light")) {
-			$("body").removeClass("light");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("light");
-			$("#switch").addClass("switched");
-		}
+jQuery(document).ready(function($){
+	//toggle 3d navigation
+	$('.cd-3d-nav-trigger').on('click', function(){
+		toggle3dBlock(!$('.cd-header').hasClass('nav-is-visible'));
 	});
-	
-})(jQuery); 
+
+	//select a new item from the 3d navigation
+	$('.cd-3d-nav a').on('click', function(){
+		var selected = $(this);
+		selected.parent('li').addClass('cd-selected').siblings('li').removeClass('cd-selected');
+		updateSelectedNav('close');
+	});
+
+	$(window).on('resize', function(){
+		window.requestAnimationFrame(updateSelectedNav);
+	});
+
+	function toggle3dBlock(addOrRemove) {
+		if(typeof(addOrRemove)==='undefined') addOrRemove = true;	
+		$('.cd-header').toggleClass('nav-is-visible', addOrRemove);
+		$('main').toggleClass('nav-is-visible', addOrRemove);
+		$('.cd-3d-nav-container').toggleClass('nav-is-visible', addOrRemove);
+	}
+
+	//this function update the .cd-marker position
+	function updateSelectedNav(type) {
+		var selectedItem = $('.cd-selected'),
+			selectedItemPosition = selectedItem.index() + 1, 
+			leftPosition = selectedItem.offset().left,
+			backgroundColor = selectedItem.data('color');
+
+		$('.cd-marker').removeClassPrefix('color').addClass('color-'+ selectedItemPosition).css({
+			'left': leftPosition,
+		});
+		if( type == 'close') {
+			$('.cd-marker').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				toggle3dBlock(false);
+			});
+		}
+	}
+
+	$.fn.removeClassPrefix = function(prefix) {
+	    this.each(function(i, el) {
+	        var classes = el.className.split(" ").filter(function(c) {
+	            return c.lastIndexOf(prefix, 0) !== 0;
+	        });
+	        el.className = $.trim(classes.join(" "));
+	    });
+	    return this;
+	};
+});
