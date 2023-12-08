32a61bd9d3b7639e6dd3fdda11e5a8a410b0b3f5
Eva Haras <usenkoeva1@gmail.com>
2023-12-08 18:28:05 +0200

    HW

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            330:{
+                items:2
+            },
+            480:{
+                items:3
+            },
+            625:{
+                items:4
+            },
+            785:{
+                items:5
+            },
+            935:{
+                items:6
+            },
+            1090:{
+                items:7
+            },
+            1250:{
+                items:8
+            },
+            1290:{
+                items:9
+            }
+        }
+});
+    $( function() {
+        $( "#accordion" ).accordion();
+      } );
+});
 jQuery(document).ready(function($){
 	//toggle 3d navigation
 	$('.cd-3d-nav-trigger').on('click', function(){
@@ -49,3 +92,4 @@ jQuery(document).ready(function($){
 	    return this;
 	};
 });
+
